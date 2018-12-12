36f0175bda2480ad52715f9a392087ce8546bedd
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-12-12 15:40:28 +0000

    4.1-4.7
    Chapter 4 Started, completed up to 4.7

---
+++ 4/4.6/BookLogger/public/app/app.js
@@ -0,0 +1,76 @@
+(function() {
+
+    var app = angular.module('app', ['ngRoute', 'ngCookies']);
+
+    app.provider('books', ['constants', function (constants) {
+      this.$get = function () {
+
+        var appName = constants.APP_TITLE;
+        var appDesc = constants.APP_DESCRIPTION;
+
+        var version = constants.APP_VERSION;
+
+        if (includeVersionInTitle) {
+          appName += ' ' + version;
+        }
+
+        return {
+          appName: appName,
+          appDesc: appDesc
+        };
+      };
+
+      var includeVersionInTitle = false;
+      this.setIncludeVersionInTitle = function (value) {
+        includeVersionInTitle = value;
+      };
+    }]);
+
+    app.config(['booksProvider', '$routeProvider', '$logProvider', function(booksProvider, $routeProvider, $logProvider) {
+
+      booksProvider.setIncludeVersionInTitle(true);
+
+      $logProvider.debugEnabled(false);
+
+      $routeProvider
+        .when('/', {
+          templateUrl: '/app/templates/books.html',
+          controller: 'BooksController',
+          controllerAs: 'books'
+        })
+        .when('/AddBook', {
+          templateUrl: '/app/templates/addBook.html',
+          controller: 'AddBookController',
+          controllerAs: 'addBook'
+        })
+        .when('/EditBook/:bookID', {
+          templateUrl: '/app/templates/editBook.html',
+          controller: 'EditBookController',
+          controllerAs: 'bookEditor'
+        })
+        .otherwise('/');
+
+    }]);
+
+    app.run(['$rootScope', function($rootScope) {
+
+      $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+        console.log('successfully changed routes');
+
+      });
+
+      $rootScope.$on('$routeChangeError', function (event, current, previous, rejection) {
+        
+        console.log('error changing routes');
+
+        console.log(event);
+        console.log(current);
+        console.log(previous);
+        console.log(rejection);
+
+      });
+
+    }])
+
+}());
